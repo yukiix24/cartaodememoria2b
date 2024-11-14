8b4e32226812bce99d897ca10409874549259e1c
yukiix <142320251+yukiix24@users.noreply.github.com>
2024-11-14 07:44:27 -0300

    cod

--- perguntas.js
+++ perguntas.js
@@ -1,15 +1,30 @@
-criaCartao(
+criaCartao{
     'ANimais de eStImação',
     'Qual é o animal mais comum do mundo?',
-    'É o cachorro',
-    ' Qual é a espectativa média de um gato doméstico?',
-    'De 16 a 20 anos.',
+    'É o cachorro'
+}
+criaCartao{
+    'ANimais de eStImação',
     'Por que os cachorros abanam o rabo?',
-    'Para expressar emoções, como por exemplo a felicidade.',
+   ' Para expressar emoções'
+}
+criaCartao{
+    'ANimais de eStImação',
     'Qual o animal doméstico que vive por mais tempo?',
-    'Cacatua',
-    'Qual é a estimativa de vida média de um hamster?',
-    'De 3 a 4 anos',
-    'Quantas raças existem de porquinho-da-índia?',
-    'Existem mais de 20 raças diferentes de porquinhos da índia',
-)
+    'Cacatua'
+}
+criaCartao{
+    'ANimais de eStImação',
+     'Qual é a estimativa de vida média de um hamster?',
+    'De 3 a 4 anos'
+}
+criaCartao{
+    'ANimais de eStImação',
+   'Quantas raças existem de porquinho-da-índia?',
+    'Existem mais de 20 raças diferentes de porquinhos da índia'
+}
+criaCartao{
+    'ANimais de eStImação',
+    ' Qual é a espectativa média de um gato doméstico?',
+    'De 16 a 20 anos.'
+}
