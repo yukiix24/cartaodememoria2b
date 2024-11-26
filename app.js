394215f2cbe4ed8d4cc8ae631418a8b87168715b
yukiix <142320251+yukiix24@users.noreply.github.com>
2024-11-26 07:36:02 -0300

    correcao

---
+++ app.js
@@ -0,0 +1,25 @@
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container')
+      let cartao = document.createElement('article')
+      cartao.className = 'cartao'
+       cartao.innerHTML = `
+            <div class="cartao_conteudo">
+                <h3>${categoria}</h3>
+                <div class="cartao_conteudo_pergunta">
+                    <p>${pergunta}</p>
+                </div>
+                <div class="cartao_conteudo_resposta">
+                    <p>${resposta}</p>
+                </div>
+            </div>  
+            `
+let respostaEstaVisivel = false
+
+function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
+
+container.appendChild(cartao)
+}
--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,30 @@
-criaCartao{
+criaCartao(
     'ANimais de eStImação',
     'Qual é o animal mais comum do mundo?',
     'É o cachorro'
-}
-criaCartao{
+)
+criaCartao(
     'ANimais de eStImação',
     'Por que os cachorros abanam o rabo?',
    ' Para expressar emoções'
-}
-criaCartao{
+)
+criaCartao(
     'ANimais de eStImação',
     'Qual o animal doméstico que vive por mais tempo?',
     'Cacatua'
-}
-criaCartao{
+)
+criaCartao(
     'ANimais de eStImação',
      'Qual é a estimativa de vida média de um hamster?',
     'De 3 a 4 anos'
-}
-criaCartao{
+)
+criaCartao(
     'ANimais de eStImação',
    'Quantas raças existem de porquinho-da-índia?',
     'Existem mais de 20 raças diferentes de porquinhos da índia'
-}
-criaCartao{
+)
+criaCartao(
     'ANimais de eStImação',
     ' Qual é a espectativa média de um gato doméstico?',
     'De 16 a 20 anos.'
-}
+)
